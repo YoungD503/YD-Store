6a8b444cb3f7da8496b9a0022b243d6c5538a87b
YoungD503 <231156994+YoungD503@users.noreply.github.com>
2026-01-21 15:51:43 -0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,7 @@
-as
+const stripe = Stripe("YOUR_PUBLIC_STRIPE_KEY");
+
+document.getElementById("buyBook")?.addEventListener("click", () => {
+  fetch("/create-checkout-session", { method:"POST" })
+    .then(res => res.json())
+    .then(data => stripe.redirectToCheckout({ sessionId:data.id }));
+});
